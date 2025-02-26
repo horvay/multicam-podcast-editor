5225ecb2f0c6ce0b0fd4ddf683137376c9ec2238
Greg Horvay <horvay@gmail.com>
2025-02-25 20:53:47 -0500

    better podcast filters

--- src/multicam_podcast_editor/audio_enhancement.py
+++ src/multicam_podcast_editor/audio_enhancement.py
@@ -12,7 +12,7 @@ def podcast_audio(vid_list, enhance_type, threads=10):
         os.remove("temp_video.mp4")
 
     filters = (
-        "afftdn=nr=12:nf=-50, anequalizer='c0 f=100 w=100 g=3 t=0, c0 f=10000 w=2000 g=3 t=0, c1 f=100 w=100 g=3 t=0, c1 f=10000 w=2000 g=3 t=0', acompressor"
+        "highpass=f=80, afftdn=nr=12:nf=-50, anequalizer='c0 f=200 w=200 g=1.5 t=0, c0 f=7000 w=2000 g=2 t=0, c1 f=200 w=200 g=1.5 t=0, c1 f=7000 w=2000 g=2 t=0', acompressor=threshold=-18dB:ratio=3:1:attack=5:release=100"
         if enhance_type == "music"
         else "afftdn=nr=12:nf=-50, acompressor"
     )
