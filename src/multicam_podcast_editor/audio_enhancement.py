035ce45d89922635f5cc62511c1a14894dcaab52
Greg <horvay@localhost>
2025-03-08 12:53:04 -0500

    added limited

--- src/multicam_podcast_editor/audio_enhancement.py
+++ src/multicam_podcast_editor/audio_enhancement.py
@@ -11,16 +11,23 @@ def podcast_audio(vid_list, enhance_type, threads=10):
     if os.path.exists("temp_video.mp4"):
         os.remove("temp_video.mp4")
 
-    filters = (
-        "highpass=f=80, afftdn=nr=12:nf=-50, anequalizer='c0 f=200 w=200 g=1.5 t=0, c0 f=7000 w=2000 g=2 t=0, c1 f=200 w=200 g=1.5 t=0, c1 f=7000 w=2000 g=2 t=0', acompressor=threshold=-18dB:ratio=3:1:attack=5:release=100"
-        if enhance_type == "music"
-        else "afftdn=nr=12:nf=-50, acompressor"
-    )
+    if enhance_type == "music":
+        filters = (
+            "highpass=f=80, afftdn=nr=12:nf=-50, anequalizer='c0 f=200 w=200 g=1.5 t=0, c0 f=7000 w=2000 g=2 t=0, c1 f=200 w=200 g=1.5 t=0, c1 f=7000 w=2000 g=2 t=0', "
+            "acompressor=threshold=-18dB:ratio=3:1:attack=5:release=50, "
+            "acompressor=threshold=-2dB:ratio=20:1:attack=1:release=100"
+        )
+    else:
+        filters = (
+            "afftdn=nr=12:nf=-50, "
+            "acompressor=threshold=-15dB:ratio=4:1:attack=5:release=50, "
+            "acompressor=threshold=-2dB:ratio=20:1:attack=1:release=100"
+        )
 
     for vid in vid_list:
         normalize = FFmpegNormalize(
             target_level=-14,
-            true_peak=-1.0,
+            true_peak=-2.0,
             progress=True,
             audio_codec="aac",
             output_format="mp4",
